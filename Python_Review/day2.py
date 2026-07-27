2271ce7dac36830431063f30a2843af1a4771f2a
Sheyda Aghazadeh <sheyda.a21@gmail.com>
2026-07-27 09:59:33 +0200

    Improved day2 solutions

--- Python_Review/day2.py
+++ Python_Review/day2.py
@@ -1,60 +1,57 @@
-list =[5, 4, 3, 2, 1]
-print(list[0])
-print(list[-1])
-list[2] = 10
-print(list)
-print(sum(list))
-def average(numbers:float) -> float:
+numbers  =[5, 4, 3, 2, 1]
+print(numbers[0])
+print(numbers[-1])
+numbers[2] = 10
+print(numbers)
+print(sum(numbers))
+def average(numbers : list[float]) -> float:
     ## Calculate the average of a list of floats ##
     average_val = sum(numbers)/len(numbers)
     return average_val
 
-print(average(list))
+print(average(numbers))
 
-def minimum(numbers : float) -> float:
+def minimum(numbers : list[float]) -> float:
     ## Find the minimum value in a list of floats ##
     minimum_val = min(numbers)
     return minimum_val
 
-print(minimum(list))
+print(minimum(numbers))
 
-def maximum(numbers : float) -> float:
+def maximum(numbers : list[float]) -> float:
     ## Find the maximum in a list of floats ##
     maximum_val = max(numbers)
     return maximum_val
 
-print(maximum(list))
+print(maximum(numbers))
 
-def is_even(numbers : int) -> bool:
+def is_even(numbers : list[int]) -> bool:
    ## Check if each component is even or odd ##
    result = []
    for number in numbers: 
       result.append(number%2 == 0)
    return result
 
-print(is_even(list))
+print(is_even(numbers))
 
-def print_even(numbers : int) -> int:
+def print_even(numbers : list[int]) -> list[int]:
     result = []
     for number in numbers:
         if number%2 == 0:
             result.append(number)
     return result
 
-print(print_even(list))
+print(print_even(numbers))
 
-def square(numbers : float) -> float:
-    result = []
-    for number in numbers:
-        result.append(number**2)
-    return result
+def square(numbers) :
+    return [number**2 for number in numbers]
 
-print(square(list))
+print(square(numbers))
 
-def sum_withouth_func(numbers : float) -> float:
+def sum_withouth_func(numbers : list[float]) -> float:
     result = 0
     for number in numbers:
         result += number
     return result
 
-print(sum_withouth_func(list))
+print(sum_withouth_func(numbers))
